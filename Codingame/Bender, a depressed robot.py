ff54ac6b01578605079d7a51102389367b5055c2
Julian Beier <jb@futureplay.de>
2016-02-21 20:10:10 +0100

    Done with Bender, a depressed Robot and slight modifications to Skynet (The Virus)

--- Codingame/Bender, a depressed robot.py
+++ Codingame/Bender, a depressed robot.py
@@ -1,156 +1,123 @@
 import sys
 import math
-
-# Auto-generated code below aims at helping you parse
-# the standard input according to the problem statement
-
+ 
 loop = False
 path = []
-town = []
-stack = []
-stackT = []
-teleport = [[-1, -1],[-1, -1]]
-bender = [[], 'SOUTH', 0]
+town = []   #will be an 2d char array, BUT CHARACTERS HERE ARE town[y][x] not x,y!!
+teleport = []               #form=[[tp1x,tp1y],[tp2x,tp2y]]
+bender = [[], 'SOUTH', 0]   #form= [[x,y], direction, berserkmode(toggle)]
+history =[]         #history of the moves for loop detection (will contain bender+townhash)
 priority = ['SOUTH', 'EAST', 'NORTH', 'WEST']
-extra = ['T', 'B', 'X', '#', 'I', 'S', 'E', 'N', 'W']
-t = 0
-modifier = False
-dic1 = {'SOUTH':[0, 1], 'EAST':[1, 0], 'NORTH':[0, -1], 'WEST':[-1, 0]}
-dic2 = {'S':'SOUTH', 'E':'EAST', 'N':'NORTH', 'W':'WEST'}
-kill = []
-
-l, c = [int(i) for i in input().split()]
-
-def appendBender(bender):
-    stack.append([])
-    stack[len(stack)-1].append([])
-    stack[len(stack)-1][0].append(bender[0][0])
-    stack[len(stack)-1][0].append(bender[0][1])
-    stack[len(stack)-1].append(bender[1])
-    stack[len(stack)-1].append(bender[2])
-    
-def appendTown(town):
-    stackT.append([])
-    for i in town:
-        stackT[len(stackT)-1].append(i)
-
+pr_short = ['S', 'E', 'N', 'W']
+groundmod=['T','I','B','S', 'E', 'N', 'W']
+wallmod = ['X','#']
+direc_to_move = {'SOUTH':[0, 1], 'EAST':[1, 0], 'NORTH':[0, -1], 'WEST':[-1, 0]}
+c_to_direc = {'S':'SOUTH', 'E':'EAST', 'N':'NORTH', 'W':'WEST'}
+ 
+def townhash(l,c):
+    #global town
+    townval=0
+    for i in range(l):
+        for j in range(c):
+            townval+=ord(town[i][j])
+    return(townval)
+ 
+   
+def warp():
+    #global bender
+    #global teleport
+    if bender[0]==teleport[0]:
+        bender[0]=teleport[1]
+    else:
+        bender[0]=teleport[0]
+ 
+def prio_reverse():
+    #global priority
+    priority.reverse()
+   
+def beertoggle():
+    #global bender
+    bender[2]=(bender[2]+1)%2
+   
+grounddic={'T':warp, 'I':prio_reverse, 'B':beertoggle}
+       
+def newpos():   #returns new bender[0], doesnt overwrite
+    #global bender
+    move=[0,0]
+    move[0]+=direc_to_move[bender[1]][0]
+    move[1]+=direc_to_move[bender[1]][1]
+    move[0]+=bender[0][0]
+    move[1]+=bender[0][1]
+    return move
+ 
+def print_town(l,c):
+    #global town
+    for i in range(l):
+        line=''
+        for j in range(c):
+            line+=town[i][j]
+        print(line, file=sys.stderr)
+   
+   
+l, c = [int(i) for i in input().split()]   
+   
 for i in range(l):
     row = input()
-    town.append(row)
-    print(row, file=sys.stderr)
-    
-    if '@' in row:
-        bender[0].append(row.find('@'))        # -> X Position
-        bender[0].append(i)                    # -> Y Position
-        
-    if t == 0 and 'T' in row:
-        teleport[0][0] = row.find('T')
-        teleport[0][1] = i
-        t = 1
-        
-    if '$' in row:
-        kill.append(row.find('$'))
-        kill.append(i) 
-        
-    if t == 1 and 'T' in row[row.find('T') + 1:]:
-        teleport[1][0] = row.find('T',teleport[0][0]+1)
-        teleport[1][1] = i
-    elif t == 1 and 'T' in row and i != teleport[0][1]:
-        teleport[1][0] = row.find('T')
-        teleport[1][1] = i
-
-
-while bender[0] != kill:
-    #print('kill: ' + str(kill),file=sys.stderr)
-    #print('bender[0]: ' + str(bender[0]),file=sys.stderr)
-    #print('modifier: ' + str(modifier),file=sys.stderr)
-    #print(stack, file=sys.stderr)
-    #print(bender, file=sys.stderr)
-    #print(stack, file=sys.stderr)
-    #print(stackT, file=sys.stderr)
-    
-    if town[bender[0][1]][bender[0][0]] not in extra or modifier == True:
-        
-        #print('Lauf/Break/Drehen', file=sys.stderr)
-        direc = dic1[bender[1]]
-        if town[bender[0][1] + direc[1]][bender[0][0] + direc[0]] == 'X' and bender[2] == 1:
-            appendBender(bender)
-            appendTown(town)
-            for x in [0, 1]: bender[0][x] += direc[x]
-            #print('Break', file=sys.stderr)
-            path.append(bender[1])
-            
-            town[bender[0][1]] = town[bender[0][1]][:bender[0][0]] + ' ' + town[bender[0][1]][bender[0][0]+1:]
-            
-            if bender in stack and stack.count(bender) > 2 and stackT[stack.index(bender)] == town:
-                print(bender, file=sys.stderr)
-                print(stack[stack.index(bender)+1], file=sys.stderr)
-                print(stack[stack.index(bender)+2], file=sys.stderr)
-                print(stack[stack.index(bender)+3], file=sys.stderr)
-                print(stack[stack.index(bender)+4], file=sys.stderr)
-                print('teleport: ' + str(teleport), file=sys.stderr)
-                stackT.pop(stack.index(bender))
-                stack.remove(bender)
-                if bender in stack and stackT[stack.index(bender)] == town:
-                    loop = True
-                    break
-            
-            #print(town[bender[0][1]],file=sys.stderr)
-        elif town[bender[0][1] + direc[1]][bender[0][0] + direc[0]] not in ['#', 'X']:
-            appendBender(bender)
-            appendTown(town)
-            for x in [0, 1]: bender[0][x] += direc[x]
-            modifier = False
-            #print('Lauf ' + bender[1], file=sys.stderr)
-            path.append(bender[1])
-            
-            if bender in stack and stack.count(bender) > 2 and stackT[stack.index(bender)] == town:
-                print(bender, file=sys.stderr)
-                print(stack[stack.index(bender)+1], file=sys.stderr)
-                print(stack[stack.index(bender)+2], file=sys.stderr)
-                print(stack[stack.index(bender)+3], file=sys.stderr)
-                print(stack[stack.index(bender)+4], file=sys.stderr)
-                print('teleport: ' + str(teleport), file=sys.stderr)
-                stackT.pop(stack.index(bender))
-                stack.remove(bender)
-                if bender in stack and stackT[stack.index(bender)] == town:
-                    loop = True
-                    break
+    town.append([])
+    for j in range(c):
+        town[i].append(row[j])
+        if row[j]=='@':
+            bender[0].append(j)     #x=j
+            bender[0].append(i)     #y=i
+        if row[j]=='T':
+            teleport.append([])
+            teleport[len(teleport)-1].append(j)
+            teleport[len(teleport)-1].append(i)
+ 
+ 
+print_town(l,c)
+ 
+ground=town[bender[0][1]][bender[0][0]]
+while ground!='$' and loop!=True:
+    #first check the ground, then the walls, then move
+    print('ground: g '+str(ground)+' g', file=sys.stderr)
+    if ground in groundmod:
+        if ground in pr_short:
+            bender[1]=c_to_direc[ground]
         else:
-            bender[1] = priority[0]
-            direc = dic1[bender[1]]
-            #print('Dreh', file=sys.stderr)
-            while town[bender[0][1] + direc[1]][bender[0][0] + direc[0]] in ['#', 'X']:
-                bender[1] = priority[(priority.index(bender[1]) + 1) % 4]
-                direc = dic1[bender[1]]
-                #print(5, file=sys.stderr)
-        
-    elif town[bender[0][1]][bender[0][0]] in ['N', 'S', 'W', 'E'] and modifier == False:
-        bender[1] = dic2[town[bender[0][1]][bender[0][0]]]
-        modifier = True
-        #print('Richtung ' + bender[1], file=sys.stderr)
-    elif town[bender[0][1]][bender[0][0]] == 'B' and modifier == False:
-        bender[2] = (bender[2]+1)%2
-        modifier = True
-        #print('Beer', file=sys.stderr)
-    elif town[bender[0][1]][bender[0][0]] == 'I' and modifier == False:
-        priority.reverse()
-        modifier = True
-        #print('Invertieren', file=sys.stderr)
-    elif town[bender[0][1]][bender[0][0]] == 'T' and modifier == False:
-        #print(9, file=sys.stderr)
-        if bender[0] == teleport[0]:
-            bender[0] = teleport[1]
-            #print('Teleport 1 -> 2', file=sys.stderr)
-        elif bender[0] == teleport[1]:
-            bender[0] = teleport[0]
-            #print('Teleport 2 -> 1', file=sys.stderr)
-        modifier = True
-    
-    
-    #print('---------------------',file=sys.stderr)
-    #print('#' + town[bender[0][1]][bender[0][0]] + '#', file=sys.stderr)
-    
+            grounddic[ground]()
+    frontpos=newpos()           #from: [x,y]
+    front=town[frontpos[1]][frontpos[0]]
+    direc=0     #index for priority list
+    print('front: f '+str(front)+' f', file=sys.stderr)
+    while front in wallmod:
+        print('WALL', file=sys.stderr)
+        if front=='X' and bender[2]==1:
+            town[frontpos[1]][frontpos[0]]=' '
+            break
+        bender[1]=priority[direc]
+        direc+=1
+        frontpos=newpos()
+        front=town[frontpos[1]][frontpos[0]]
+ 
+    bender[0]=newpos()
+    ground=town[bender[0][1]][bender[0][0]]
+    path.append(bender[1])
+   
+    #loop detection:
+    historyentry=[]
+    historyentry.append([])
+    historyentry[0].append(bender[0][0])
+    historyentry[0].append(bender[0][1])
+    historyentry.append(bender[1])
+    historyentry.append(bender[2])
+    historyentry.append(townhash(l,c))
+    if historyentry not in history:
+        history.append(historyentry)
+    else:
+        loop=True
+    print(history, file=sys.stderr)
+   
 if loop == True:
     print('LOOP')
 else:
--- Codingame/Skynet (The Virus).py
+++ Codingame/Skynet (The Virus).py
@@ -1,81 +1,87 @@
 import sys
 import math
 import random
-
+ 
 # Auto-generated code below aims at helping you parse
 # the standard input according to the problem statement.
-
+ 
 # n: the total number of nodes in the level, including the gateways
 # l: the number of links
 # e: the number of exit gateways
-visited = []
-erg = []
-abstand = 1
+#visited = []
+#erg = []
 adj = []
 gateways = []
-
+erg = []
+ 
 def delZelda(n1, n2):  
     adj[n1].remove(n2)
     adj[n2].remove(n1)
-
+ 
 def addAdj(n1, n2):
     adj[n1].append(n2)
     adj[n2].append(n1)
-
+ 
 def calcAbstand(start):
-    global abstand
-
+    abstand = 1
+ 
     visited.append(start)
     temp = start
     erg[start] = 0
-
+ 
     for node in visited:
         for x in adj[node]:
             if x not in visited:
                 visited.append(x)
                 erg[x] = abstand
-
+ 
         if node == temp:
             abstand += 1
             temp = visited[len(visited)-1]
-
+ 
 def calcAction():
     global n
     mini = n + 1
     g = -1
     current = -1
-
+ 
     for gateway in gateways:
         for node in adj[gateway]:
             if erg[node] < mini:
                 mini = erg[node]
                 g = gateway
                 current = node
-
+ 
     delZelda(g, current)
     print(str(g) + ' ' + str(current))
-
+ 
 n, l, e = [int(i) for i in input().split()]
-
+ 
 for x in range(n):
-    erg.append(-1)
+    #erg.append(-1)
     adj.append([])
-
+ 
 for i in range(l):
     # n1: N1 and N2 defines a link between these nodes
     n1, n2 = [int(j) for j in input().split()]
     addAdj(n1, n2)
-
+ 
 for i in range(e):
     gateways.append(int(input()))  # the index of a gateways node
-
+ 
 # game loop
 while True:
+    visited = []
+    for x in range(n):
+        erg.append(-1)
     si = int(input())  # The index of the node on which the Skynet agent is positioned this turn
     calcAbstand(si)
+    print(si, file=sys.stderr)
+    print(erg, file=sys.stderr)
     calcAction()
+    erg.clear()
     # Write an action using print
-    # To debug: print("Debug messages...", file=sys.stderr)
-
+    # To debug: print("Debu messages...", file=sys.stderr)
+ 
     # Example: 0 1 are the indices of the nodes you wish to sever the link between
 #   print("0 1")
